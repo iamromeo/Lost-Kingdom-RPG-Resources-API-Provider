f5043c9c413d394d708d9589e1fcca6ef8e08565
Dimitris Havlidis <hello@iamromeo.com>
2016-11-11 12:13:09 +0000

    Adding distribution config file

---
+++ config-dist.php
@@ -0,0 +1,13 @@
+	<?php
+$app['debug'] = true;
+$app['log.level'] = Monolog\Logger::DEBUG;
+// $app['log.level'] = Monolog\Logger::ERROR;
+$app['api.version'] = "v1";
+$app['api.endpoint'] = "/api";
+// Database Credentials
+$db_host = '';
+$db_user = '';
+$db_password = '';
+$db_name = '';
+
+?>
